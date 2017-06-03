94ed36e695e70f59623e24f190061a7e9ce9b871
DanYellow <jeanlouis.danielo@sfr.fr>
2017-06-03 15:46:18 +0200

    adds more unit tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # unit-tests-for-tech-review
 A bunch of very bad written unit tests for candidates
+
+
+Make an interview to get a developer can be hard and test his skill can be difficult. You can ask him
--- __tests__/es6.js
+++ __tests__/es6.js
@@ -1,47 +1,28 @@
 const TITLE = 'EcmaScript Next';
 
 describe(TITLE, () => {
-  // test(`02 - ${TITLE}`, () => {
-    
-
-    
-
-  //   async function fetchAPI () {
-  //     let resultAPI = await delayedLocalAPI();
-  //   };
-  //   fetchAPI().then((response) => {
-  //     expect(data).toEqual(myLocalAPI)
-  //   });
-
-
-
-
-  // });
-  
-  test('works with async/await', async () => {
-    const delayedLocalAPI = () => (
-      new Promise(resolve => {
-        const myLocalAPI = {
-          firstname: 'Bruce',
-          lastname: 'Wayne',
-          secret_identity: 'Batman'
-        };
-
-        setTimeout(() => {
-          resolve(myLocalAPI)
-        }, 0)
-      })
-    )
-    
-    expect.assertions(1);
-    const data = await delayedLocalAPI(4);
-    expect(data).toEqual(myLocalAPI);
-  });
-
   test(`03 - ${TITLE}`, () => {
     const myArray = [1, 3, 4, 6, 2, 6, 7, 9, 0];
 
     expect($).toBe([1]);
     expect($).toBe([3, 4, 6, 2, 6, 7, 9, 0]);
+  });
+
+  test(`10 - ${TITLE}`, async () => {
+    const myLocalAPI = {
+          firstname: 'Bruce',
+          lastname: 'Wayne',
+          secret_identity: 'Batman'
+        };
+
+    const delayedLocalAPI = () => (
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve(myLocalAPI)
+        }, 0)
+      })
+    )
+
+    expect($).toEqual(myLocalAPI);
   });
 });
